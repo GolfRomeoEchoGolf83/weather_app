a28ecfc84d76f5deb3dcefb3c43716fb88ed99f0
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-05 15:30:09 +0100

    Creating first page

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('weather', ['ionic'])
 
-  .config(function($stateProvider, $urlRouteProvider){
+  .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('weather', {
         url: '/weather',
@@ -13,16 +13,15 @@ angular.module('weather', ['ionic'])
         controller: 'WeatherCtrl'
       });
 
-    $urlRouteProvider.otherwise('/weather');
+    $urlRouterProvider.otherwise('/weather');
   })
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
--- www/js/constants.js
+++ www/js/constants.js
@@ -1,11 +1,13 @@
 /**
  * Created by Greg on 05/01/2017.
  */
+
 angular.module('weather')
 
-.constant('OpenWeatherConfig', {
-  searchUrl: 'http://api.openweathermap.org/data/2.5/weather?q=',
-  units: '&units=metric',
-  appid: '&appid=<ae655732a80e61ceb5461dd6967fbae3>',
-  imgUrl: 'http://openweathermap.org/img/w/'
-})
+  .constant('OpenWeatherConfig', {
+    searchUrl: 'http://api.openweathermap.org/data/2.5/weather?q=',
+    units: '&units=metric',
+    appid: '&appid=<ae655732a80e61ceb5461dd6967fbae3>',
+    imgUrl: 'http://openweathermap.org/img/w/'
+  });
+
